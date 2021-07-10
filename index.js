a2cf71982f209bd069c4b4ef5e00e709f430b4f2
Leonnoirsr <huggins.leon@gmail.com>
2021-07-10 15:29:53 -0400

    Reorganized index.js in the order of CRUD. Added Delete button on show page via a form action/method overrid

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const songs = [
+let songs = [
 	{ id: uuid(), artist: 'Leon Noir', song: 'Figure it out' },
 	{ id: uuid(), artist: 'Leon Noir', song: 'Like That' },
 	{ id: uuid(), artist: 'Leon Noir', song: 'Vibe Out' },
@@ -28,16 +28,36 @@ app.get('/songs', (req, res) => {
 	res.render('songs/index', { songs });
 });
 
-//Create(Get) --- New ( Read)
-app.get('/songs/new', (req, res) => {
-	res.render('songs/new');
-});
+
 //Create(Post) --- Create
 app.post('/songs', (req, res) => {
 	const { artist, song } = req.body;
 	songs.push({ artist, song, id: uuid() });
 	res.redirect('/songs');
 });
+
+//Create(Get) --- New ( Read)
+app.get('/songs/new', (req, res) => {
+	res.render('songs/new');
+});
+
+// Edit --- Update
+app.patch('/songs/:id', (req, res) => {
+	const { id } = req.params;
+	const newSongText = req.body.song;
+	const foundSong = songs.find((song) => song.id === id);
+	foundSong.song = newSongText;
+	res.redirect('/songs');
+});
+
+//Destroy --- Delete
+app.delete('/songs/:id', (req, res) => {
+	const { id } = req.params;
+	const foundSong = songs.find((song) => song.id === id);
+	songs = songs.filter(song => song.id !== id);
+	 res.redirect('/songs');
+});
+
 
 //Show ---
 app.get('/songs/:id', (req, res) => {
@@ -46,21 +66,14 @@ app.get('/songs/:id', (req, res) => {
 	res.render('songs/show', { song });
 });
 
-// Edit --- Update
+// Edit --- Updated view
 app.get('/songs/:id/edit', (req, res) => {
 	const { id } = req.params;
 	const song = songs.find((song) => song.id === id);
 	res.render('songs/edit', { song });
 });
 
-// Edit ---
-app.patch('/songs/:id', (req, res) => {
-	const { id } = req.params;
-	const newSongText = req.body.song;
-	const foundSong = songs.find((song) => song.id === id);
-	foundSong.song = newSongText;
-	res.redirect('/songs');
-});
+
 
 app.listen(port, () => {
 	console.log('Listening on 1017, Bow!');
